7d564844c1ebfaa36759ae1e0d8755868a2dd501
1793907272 <1793907272@qq.com>
2016-10-27 06:57:35 +0800

    #1：修改之前的类错误。

--- src/main/java/com/tieba/onsn/PenguinLive.java
+++ src/main/java/com/tieba/onsn/PenguinLive.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 package com.tieba.onsn;
-
 
 import com.tieba.onsn.swing.MainFrame;
 
@@ -21,78 +19,31 @@ public class PenguinLive {
         setUIFont();
         new PenguinLive().start();
     }
-    public void start () {
+
+    public void start() {
 
         mainFrame.setVisible(true);
     }
-    public void post (String type) {
+
+    public void post(String type) {
         switch (type) {
 
         }
     }
 
-    public static void setUIFont () {
-        Font f = new Font("微软雅黑",Font.PLAIN,18);
+    public static void setUIFont() {
+        Font f = new Font("微软雅黑", Font.PLAIN, 18);
 
-        String   names[]={ "Label", "CheckBox", "PopupMenu","MenuItem", "CheckBoxMenuItem",
-                "JRadioButtonMenuItem","ComboBox", "Button", "Tree", "ScrollPane",
+        String names[] = {"Label", "CheckBox", "PopupMenu", "MenuItem", "CheckBoxMenuItem",
+                "JRadioButtonMenuItem", "ComboBox", "Button", "Tree", "ScrollPane",
                 "TabbedPane", "EditorPane", "TitledBorder", "Menu", "TextArea",
                 "OptionPane", "MenuBar", "ToolBar", "ToggleButton", "ToolTip",
                 "ProgressBar", "TableHeader", "Panel", "List", "ColorChooser",
-                "PasswordField","TextField", "Table", "Label", "Viewport",
-                "RadioButtonMenuItem","RadioButton", "DesktopPane", "InternalFrame"
+                "PasswordField", "TextField", "Table", "Label", "Viewport",
+                "RadioButtonMenuItem", "RadioButton", "DesktopPane", "InternalFrame"
         };
         for (String item : names) {
-            UIManager.put(item+ ".font",f);
+            UIManager.put(item + ".font", f);
         }
     }
-=======
-package com.tieba.onsn;
-
-
-import com.tieba.onsn.swing.MainFrame;
-
-import javax.swing.*;
-import java.awt.*;
-
-
-/**
- * Created by Onsn on 2016/10/21.
- *
- * @author OnSN
- */
-public class PenguinLive {
-    public static final Font YaHei = new Font("微软雅黑", Font.BOLD, 15);
-    private MainFrame mainFrame = new MainFrame();
-
-    public static void main(String[] args) {
-        setUIFont();
-        new PenguinLive().start();
-    }
-    public void start () {
-
-        mainFrame.setVisible(true);
-    }
-    public void post (String type) {
-        switch (type) {
-
-        }
-    }
-
-    public static void setUIFont () {
-        Font f = new Font("微软雅黑",Font.PLAIN,18);
-
-        String   names[]={ "Label", "CheckBox", "PopupMenu","MenuItem", "CheckBoxMenuItem",
-                "JRadioButtonMenuItem","ComboBox", "Button", "Tree", "ScrollPane",
-                "TabbedPane", "EditorPane", "TitledBorder", "Menu", "TextArea",
-                "OptionPane", "MenuBar", "ToolBar", "ToggleButton", "ToolTip",
-                "ProgressBar", "TableHeader", "Panel", "List", "ColorChooser",
-                "PasswordField","TextField", "Table", "Label", "Viewport",
-                "RadioButtonMenuItem","RadioButton", "DesktopPane", "InternalFrame"
-        };
-        for (String item : names) {
-            UIManager.put(item+ ".font",f);
-        }
-    }
->>>>>>> origin/master
 }
--- src/main/java/com/tieba/onsn/swing/JButtonX.java
+++ src/main/java/com/tieba/onsn/swing/JButtonX.java
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
 package com.tieba.onsn.swing;
-
 import javax.swing.*;
 import java.beans.ConstructorProperties;
+
 
 /**
  * Created by Onsn on 2016/10/26.
  *
  * @author OnSN
  */
-public class JButtonX extends JButton {
+class JButtonX extends JButton {
     /**
      * Creates a button with no set text or icon.
      */
@@ -60,66 +59,3 @@ public class JButtonX extends JButton {
         setFocusable(false);
     }
 }
-=======
-package com.tieba.onsn.swing;
-
-import javax.swing.*;
-import java.beans.ConstructorProperties;
-
-/**
- * Created by Onsn on 2016/10/26.
- *
- * @author OnSN
- */
-public class JButtonX extends JButton {
-    /**
-     * Creates a button with no set text or icon.
-     */
-    public JButtonX() {
-        this(null, null);
-    }
-
-    /**
-     * Creates a button with an icon.
-     *
-     * @param icon  the Icon image to display on the button
-     */
-    public JButtonX(Icon icon) {
-        this(null, icon);
-    }
-
-    /**
-     * Creates a button with text.
-     *
-     * @param text  the text of the button
-     */
-    @ConstructorProperties({"text"})
-    public JButtonX(String text) {
-        this(text, null);
-    }
-
-    /**
-     * Creates a button where properties are taken from the
-     * <code>Action</code> supplied.
-     *
-     * @param a the <code>Action</code> used to specify the new button
-     *
-     * @since 1.3
-     */
-    public JButtonX(Action a) {
-        this();
-        setAction(a);
-    }
-
-    /**
-     * Creates a button with initial text and an icon.
-     *
-     * @param text  the text of the button
-     * @param icon  the Icon image to display on the button
-     */
-    public JButtonX(String text, Icon icon) {
-        super(text, icon);
-        setFocusable(false);
-    }
-}
->>>>>>> origin/master
